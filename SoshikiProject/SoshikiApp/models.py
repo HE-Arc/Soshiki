73d32e082bbea8a3945fd2a136c19bf975547573
Pedro Costa <jules007.cesar@hotmail.com>
2018-02-14 18:36:30 +0100

    CRUD for Tables and generic TemplateViews

---
+++ SoshikiProject/SoshikiApp/models.py
@@ -0,0 +1,94 @@
+from django.db import models
+
+class Table(models.Model):
+	"""
+	Table for the tangan that will contain your
+	organisation for one project.
+	"""
+	name =  models.CharField(max_length=200)
+	favorite = models.BooleanField(default=False)
+	private = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
+
+class List(models.Model):
+	"""
+	Lists are sections of the Table.
+	They'll contain cards that will be the tasks to do.
+	"""
+	name = models.CharField(max_length=200)
+	position = models.IntegerField()
+	table = models.ForeignKey(Table, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Card(models.Model):
+	"""
+	Cards that will be the tasks to do.
+	They'll contain a title, a description,
+	a deadline, some checklists, labels, etc... 
+	"""
+	title = models.CharField(max_length=200)
+	deadline = models.DateTimeField('Deadline')
+	description = models.TextField()
+	file = models.URLField()
+	position = models.IntegerField()
+	list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+class CheckList(models.Model):
+	"""
+	List of tasks that are checkable.
+	"""
+	name = models.CharField(max_length=200)
+	card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class OptionsList(models.Model):
+	"""
+	Value of a checklist's element.
+	"""
+	value = models.CharField(max_length=300)
+	checkList = models.ForeignKey(CheckList, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.value
+
+class Label(models.Model):
+	"""
+	A label is a text with a colorfull background.
+	"""
+	name = models.CharField(max_length=200)
+	color = models.CharField(max_length=7)
+	card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
+class Comment(models.Model):
+	"""
+	A card can contain several comments by the members of the table.
+	"""
+	value = models.TextField()
+	date = models.DateTimeField('published date')
+	card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.value
+
+class Member(models.Model):
+	"""
+	A member is a person that is invited to colaborate in this table.
+	"""
+	name = models.CharField(max_length=200)
+	username = models.CharField(max_length=200)
+	table = models.ForeignKey(Table, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name + ' (' + self.username + ')'
---
+++ SoshikiProject/SoshikiApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard', views.DashboardView.as_view(), name='dashboard'),
+
+    path('dashboard/tables', views.TablesListView.as_view(), name='dashboard-tables'),
+    path('dashboard/tables/<pk>/', views.TableDetailView.as_view(), name='table-detail'),
+    path('dashboard/tables/create', views.TableCreateView.as_view(), name='table-create'),
+    path('dashboard/tables/<pk>/update', views.TableUpdateView.as_view(), name='table-update'),
+    path('dashboard/tables/<pk>/delete', views.TableDeleteView.as_view(), name='table-delete'),
+]
