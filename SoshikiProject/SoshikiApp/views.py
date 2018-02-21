3961ba49c34cb1264b73af4c5999e55ffadc58a2
Pedro Costa <jules007.cesar@hotmail.com>
2018-02-21 15:23:05 +0100

    Modification of the Comment Model with the link of the creator

--- SoshikiProject/SoshikiApp/views.py
+++ SoshikiProject/SoshikiApp/views.py
@@ -5,19 +5,22 @@ from django.urls import reverse_lazy
 
 from .models import Table
 
+
 def index(request):
-	context = {}
-	return render(request, 'SoshikiApp/index.html', context)
+    context = {}
+    return render(request, 'SoshikiApp/index.html', context)
+
 
 class DashboardView(generic.TemplateView):
-	template_name = "SoshikiApp/dashboard.html"
+    template_name = "SoshikiApp/dashboard.html"
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		context['tables'] = Table.objects.all()
-		context['table_private'] = Table.objects.filter(private=True).all()
-		context['table_favorite'] = Table.objects.filter(favorite=True).all()
-		return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tables'] = Table.objects.all()
+        context['table_private'] = Table.objects.filter(private=True).all()
+        context['table_favorite'] = Table.objects.filter(favorite=True).all()
+        return context
+
 
 class TablesListView(generic.ListView):
     model = Table
@@ -25,19 +28,23 @@ class TablesListView(generic.ListView):
     def get_queryset(self):
         return Table.objects.all()
 
+
 class TableDetailView(generic.DetailView):
     model = Table
+
 
 class TableCreateView(generic.CreateView):
     model = Table
     fields = ['name', 'favorite', 'private']
     success_url = reverse_lazy('tables-list')
 
+
 class TableUpdateView(generic.UpdateView):
-	model = Table
-	fields = ['name', 'favorite', 'private']
-	success_url = reverse_lazy('tables-list')
+    model = Table
+    fields = ['name', 'favorite', 'private']
+    success_url = reverse_lazy('tables-list')
+
 
 class TableDeleteView(generic.DeleteView):
-	model = Table
-	success_url = reverse_lazy('tables-list')
+    model = Table
+    success_url = reverse_lazy('tables-list')
