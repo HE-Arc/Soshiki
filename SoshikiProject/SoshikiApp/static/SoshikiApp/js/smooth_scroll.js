7d1f16dd861e1fc5bcb7568acaf7ee78395395ee
Pedro Costa <jules007.cesar@hotmail.com>
2018-02-26 10:14:03 +0100

    Add a better desgin for the home page

---
+++ SoshikiProject/SoshikiApp/static/SoshikiApp/js/smooth_scroll.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#lm').on('click', function(evt){
+        evt.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').stop().animate({scrollTop: $(target).offset().top}, 1000 );
+    });
+});
