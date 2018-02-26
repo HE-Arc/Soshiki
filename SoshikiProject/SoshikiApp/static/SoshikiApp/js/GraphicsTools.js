89f170ac11f262bc5921c7ed81385e8ed3451516
Pedro Costa <jules007.cesar@hotmail.com>
2018-02-26 12:05:01 +0100

    Make the card clickable

--- SoshikiProject/SoshikiApp/static/SoshikiApp/js/GraphicsTools.js
+++ SoshikiProject/SoshikiApp/static/SoshikiApp/js/GraphicsTools.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
         var target = $(this).attr('href');
         $('html, body').stop().animate({scrollTop: $(target).offset().top}, 1000 );
     });
+    $('.tables').click(function () {
+        window.location = $(this).find("a").attr("href");
+        return false;
+    })
 });
