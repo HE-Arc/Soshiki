0bfdb48f7a94a85dd23359d93b8dc955980c54b4
Christophe Hirschi <christophe.hirschi@he-arc.ch>
2018-04-06 15:59:01 +0200

    Logo

--- SoshikiProject/SoshikiApp/forms.py
+++ SoshikiProject/SoshikiApp/forms.py
@@ -5,10 +5,10 @@ from .models import Card, List, Table, Comment
 
 
 class SignupForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'User name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Confirmation'}))
 
     class Meta:
         model = User
